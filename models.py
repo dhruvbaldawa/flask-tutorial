05c4a079c1400719d117b8b1c193f76a128e4c55
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2012-12-08 17:05:57 +0530

    Storing hashed passwords now

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
 
 
@@ -8,8 +9,18 @@ class User(db.Model):
     __tablename__ = 'users' # This will be the name of the table in the database
     id = db.Column('id', db.Integer, primary_key=True)
     username = db.Column('username', db.String(255), unique=True, nullable=False)
-    password = db.Column('password', db.String(255), nullable=False)
+    _password = db.Column('password', db.String(255), nullable=False)
     name = db.Column('name', db.String(255), nullable=False)
+
+    def _get_password(self):
+        return self._password
+
+    def _set_password(self, password):
+        self._password = generate_password_hash(password)
+
+    # Hide password encryption by exposing password field only.
+    password = db.synonym('_password',
+                          descriptor=property(_get_password, _set_password))
 
     @classmethod
     def authenticate(cls, username, password):
@@ -33,7 +44,7 @@ class User(db.Model):
         user = User.query.filter_by(username=username).first()
         if user is not None:
             # Check if the user has correct password
-            if user.password == password:
+            if check_password_hash(user.password, password):
                 return True, {"name": user.name, 
                     "message": "Login successful"}
             else:
